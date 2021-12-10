5c72ec5c30589dbfdbe5b338da94ba6b0189854c
TraceyHolinka <tracey.holinka@gmail.com>
2021-12-10 10:50:06 -0500

    First pass

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react'
+import {fetchPopularMovies} from './services/movies-db-api'
+import styles from './App.module.css'
+import Header from './components/Header'
+import Movies from './components/Movies'
+import Footer from './components/Footer'
+import Member from "./components/Member";
 
 function App() {
+  const COMPANY = 'Top Three Flicks'
+  const [movies, setMovies] = useState([])
+  const getMovies = async () => {
+    try {
+      const data = await fetchPopularMovies()
+      setMovies(data.results)
+    } catch (error) {
+      console.log('Error: ', error)
+    }
+  }
+
+  useEffect(() => {
+    getMovies()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      <Header company={COMPANY} />
+      <Movies movies={movies.slice(0,3)} />
+      <Member />
+      <Footer company={COMPANY}  />
     </div>
-  );
+  )
 }
 
 export default App;
